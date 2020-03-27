8b057a926ce712e4c3adab0672d55a39d3e721a5
Frank <alchemistWu0521@gmail.com>
2020-03-27 10:24:44 -0400

    add boarder

--- src/preprocess.py
+++ src/preprocess.py
@@ -127,7 +127,9 @@ def prepare_tissue_image(input_folder, output_folder, color_channel="b", section
         return ((hour * 100) + min) * 100 + sec
     img_paths.sort(key=sort_key)
 
-    raw_images = [cv2.imread(item) for item in img_paths]
+    # raw_images = [cv2.imread(item) for item in img_paths]
+        top, bottom, left, right = 400, 100, 100, 100
+    raw_images = [cv2.copyMakeBorder(cv2.imread(item), top, bottom, left, right, cv2.BORDER_CONSTANT, None, (0, 0, 0)) for item in img_paths] 
     stack_length = len(raw_images)
     pos = 0
     cv2.namedWindow("Window")
